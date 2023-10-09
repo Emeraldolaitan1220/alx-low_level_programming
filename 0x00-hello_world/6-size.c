23676a6f20e94588faa08de898aa3ba69c2d36f1
Emeraldolaitan1220 <emeraldolaitan@gmail.com>
2023-10-09 16:30:35 -0700

    finish

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - printing the sizeof
- * discription :sizeof of  various types
- * return :always 0 success
+ * main - Entry point
+ * Discription :sizeof of  various types
+ * Return: Always 0 success
  */
 int main(void)
+
 {
 	printf("Size of char: %zu bytes\n", sizeof(char));
-        printf("Size of short: %zu bytes\n", sizeof(short));
-        printf("Size of int: %zu bytes\n", sizeof(int));
-        printf("Size of long: %zu bytes\n", sizeof(long));
-        printf("Size of float: %zu bytes\n", sizeof(float));
-        printf("Size of double: %zu bytes\n", sizeof(double));
-        printf("Size of long double: %zu bytes\n", sizeof(long double));
+	printf("Size of short: %zu bytes\n", sizeof(short));
+	printf("Size of int: %zu bytes\n", sizeof(int));
+	printf("Size of long: %zu bytes\n", sizeof(long));
+	printf("size of float: %zu bytes\n", sizeof(float));
 	printf("size of long int: %zu bytes\n", sizeof(long int));
-	return(0);
+	printf("size of double: %zu bytes\n", sizeof(double));
+	printf("size of long long int: %zu bytes\n", sizeof(long long int));
+	return (0);
 }
