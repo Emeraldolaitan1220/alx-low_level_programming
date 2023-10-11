b148154b0585b2cd1e2ddb9911db0ad72de8fad4
Emeraldolaitan1220 <emeraldolaitan@gmail.com>
2023-10-10 19:22:00 -0700

    horjay

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - fibonacci <3
+ *
+ * Purpose - no hardcode
+ *
+ * Return:  (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 0; ch < 10; ch ++)
+	{
+		putchar(ch);
+		ch++;
+		return (0);
+	}
+}
+
